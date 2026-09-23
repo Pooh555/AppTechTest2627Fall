3c53b2eb325938607279d1a6683bc24a63f311d9
Pooh555 <peerasit.lu@gmail.com>
2026-09-23 22:10:21 +0800

    Configure Metro for SQLite web assets

--- metro.config.js
+++ metro.config.js
@@ -27,5 +27,6 @@ config.resolver.unstable_conditionNames = ["require", "default", "browser"]
 // This helps support certain popular third-party libraries
 // such as Firebase that use the extension cjs.
 config.resolver.sourceExts.push("cjs")
+config.resolver.assetExts.push("wasm")
 
 module.exports = config
